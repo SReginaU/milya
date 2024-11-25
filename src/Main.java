4894a871df69ae3d2d807522baac902f0ea8ec17
Regina <taipov.radik@yandex.ru>
2024-11-26 00:22:24 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 15450;
+        int accrual = 20;
+        int bonus = 1;
+
+        int miles = price / accrual * bonus;
+        System.out.println("Количество начисленных миль за купленный билет: " + miles);
+    }
+}
